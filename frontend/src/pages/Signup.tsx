3a7aa3df9522c70430967b69247fb6ab9befb520
Aadarsh Desai <adidesai91@gmail.com>
2025-03-16 18:17:58 -0400

    Initialized frontend

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,12 @@
+import { Quote } from "../components/Quote"
+
+export function Signup() {
+    return <>
+    <div className="grid grid-cols-2">
+        <div></div>
+        <div className="invisible lg:visible">
+            <Quote/>
+        </div>
+    </div>
+    </>
+}
